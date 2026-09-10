d2084c8842eb643c9327082cbe077cfc22d96d68
Sarbin Tharu <sarbintharu852@gmail.com>
2026-09-10 14:23:43 +0545

    Pushed

---
+++ Java Script/script.js
@@ -0,0 +1,267 @@
+// console.log("Hello world")
+
+// data types 
+
+//string
+
+// x = "hello"
+// y = 'hi'
+
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof "Ram")
+// console.log(x.length)
+
+
+// // Integer, Number
+
+// a = 5
+// console.log(typeof a)
+
+// b = "6"
+// console.log(typeof b)
+
+// statement = true
+
+// console.log(typeof statement)
+
+// Variable declaration
+// let, var, const
+
+// {
+//    let naam = 1
+//    console.log(naam)
+// }
+
+// console.log(naam)
+
+// {
+//    var naam = 1
+//    console.log(naam)
+// }
+
+// console.log(naam)
+
+// let x = 5;
+// const y = 5;
+
+// x = 6;
+// console.log(x)
+
+// y = 6;
+// console.log(y)
+
+// pre increment
+
+// let x = 5
+// console.log(++x)
+
+// //  post increment
+
+// let y = 5
+// console.log(y++)
+// console.log(y)
+
+// operators
+
+// arithemtic operators
+
+// + - * / %
+
+// console.log(1+2)
+// x = 1
+// y = 2
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+
+// console.log(5/2)
+// console.log(5%2)
+
+
+//  Logical Operators
+
+// AND OR NOT
+
+//  AND -> &&
+// OR -> ||
+// NOT -> !
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && false)
+
+// console.log(true ||true)
+// console.log(true ||false)
+// console.log(false || false)
+
+// console.log(!true)
+// console.log(!false)
+
+
+// relational operator
+
+// ==, >, <, >=, <=, !=, ===
+
+// console.log(5 == "5")
+// console.log(5 === "5")
+
+// console.log(4>5)
+// console.log(4<5)
+
+// console.log(4<=5)
+// console.log(5<=5)
+
+// console.log(4!= 5)
+// console.log(5!=5)
+
+// if(false){
+//     console.log("if block executed")
+
+// }else if(false){
+//     console.log("elseif block executed")
+// }
+// else{
+//     console.log("else block executed")    
+// }
+
+
+// const number = 0
+
+// if(number>0){
+//     console.log<("positive")
+
+// }
+// else if(number>0){
+//     console.log("negative")
+// }
+// else if(number == 0){
+//   console.log("zero")
+// }
+
+// let age = 20
+
+// if(age>50){
+//     console.log("senior citizen")
+// }else{
+//     if(age>=20){
+//         console.log("adult")
+//     }else{
+//         console.log("minor")
+//     }
+// }
+
+// task
+
+// check eligiblity to give exam
+// 1 => fee cleared or not, scholarship->eligible
+// 2 => attendance fulfill, not -> leaveinformed
+// 3 => backlog
+// 4 => want to give exam
+
+// feecleared = true, false
+// scholarshipGranted = true, false
+// attendancefullfill = true, false
+
+// var wannaGiveExam = true;
+// var backlog = true;
+// var attendanceFulfill = true;
+// var feeCleared = false;
+// var scholarshipGranted = false;
+
+// if (wannaGiveExam == true) {
+
+//     if (feeCleared == true || scholarshipGranted == true) {
+
+//         if (attendanceFulfill == true) {
+
+//             if (backlog == false) {
+//                 console.log("Eligible for exam");
+//             } else {
+//                 console.log("You have backlog");
+//             }
+
+//         } else {
+//             console.log("Attendance not fulfilled - Leave informed");
+//         }
+
+//     } else {
+//         console.log("Fee not cleared");
+//     }
+
+// } else {
+//     console.log("You don't want to give the exam");
+// }
+
+// ternary operator
+
+// if(condition){
+
+// }else{
+
+// }
+
+// (condition ? "first statement" : "second statement")
+
+// let x = (4>5) ? "true" : "false"
+
+// console.log(x)
+
+// switch
+// initialize var
+
+// var ticketNumber = 200
+
+// switch(ticketNumber){
+//     case 100:
+//         console.log("bahubali movie")
+//         break
+//     case 200:
+//         console.log("Gajani")
+//         break
+//     case 300:
+//         console.log("KGF")
+//         break
+//     default:
+//         console.log("invalid ticket number")
+//         break
+// }
+
+// string
+
+// let myString = "i like that car's color"
+// console.log(typeof myString)
+// console.log(myString)
+
+// let sentence = `i like that car's color "black"`
+// console.log(typeof sentence)
+// console.log(sentence)
+
+// concatenation / interpolation
+
+//  add two strings
+
+// let fName = "keshab"
+// let lName = "bc"
+
+// let fullName = fName +" "+ lName
+// console.log(fullName)
+
+// let rollNo = 5
+// let marks = 3.00
+
+// console.log("Student whose roll number is " + rollNo +"got GPA "+marks)
+
+// console.log(`student whose roll number is ${rollNo} got GPA ${marks}`)
+
+// let dsa = 40
+// let dbms = 45
+
+// console.log(dsa + dbms)
+// console.log("he got total marks: "+(dsa+dbms))
+// console.log(`he got total marks: ${dsa + dbms}`)
+
+
+
+console.log("Hello World")
